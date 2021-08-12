cb0ef3d0fb8ee51008b555374645961065d5b0f7
Vihanga Jayalath <vihangajayalath@192-168-1-103.tpgi.com.au>
2021-08-12 19:09:10 +1000

    algos

--- AlgoTrading/snp_500.py
+++ AlgoTrading/snp_500.py
@@ -1,6 +1,7 @@
 ###########################################################################################################################
 ## This program sends GET requests to the iexcloud.io Sandbox API and obtains data on stock prices.
-## It then calculates the best number of shares to buy based on the data collected.
+## Using the protfolio value obtained from the user, it then calculates the best number of shares to buy 
+## based on the data collected.
 ## Finally, this progrm returns the best number of shares to buy on the S & P 500 index as an Excel file.
 ###########################################################################################################################
 
@@ -99,14 +100,61 @@ def calculate_no_shares_to_buy(stocks_df):
     position_size = float(portfolio_size) / len(stocks_df.index)
 
     for i in range(0, len(stocks_df['Ticker'])-1):
-        stocks_df.loc[i, 'Number Of Shares to Buy'] = math.floor(position_size / stocks_df['Stock Price'][i])
+        stocks_df.loc[i, 'Number of Shares to Buy'] = math.floor(position_size / stocks_df['Stock Price'][i])
     
     print(stocks_df)
     return stocks_df
 
 
 
+def generate_xlsx_file(stocks_df):
+    writer = pd.ExcelWriter('recommended_trades.xlsx', engine='xlsxwriter')
+    stocks_df.to_excel(writer, sheet_name='Recommended Trades', index = False)
+    
+    background_color = '#0a0a23'
+    font_color = '#ffffff'
+
+    string_format = writer.book.add_format(
+            {
+                'font_color': font_color,
+                'bg_color': background_color,
+                'border': 1
+            }
+        )
+
+    dollar_format = writer.book.add_format(
+            {
+                'num_format':'$0.00',
+                'font_color': font_color,
+                'bg_color': background_color,
+                'border': 1
+            }
+        )
+
+    integer_format = writer.book.add_format(
+            {
+                'num_format':'0',
+                'font_color': font_color,
+                'bg_color': background_color,
+                'border': 1
+            }
+        )
+
+    column_formats = { 
+                        'A': ['Ticker', string_format],
+                        'B': ['Price', dollar_format],
+                        'C': ['Market Capitalization', dollar_format],
+                        'D': ['Number of Shares to Buy', integer_format]
+                        }
+
+    for column in column_formats.keys():
+        writer.sheets['Recommended Trades'].set_column(f'{column}:{column}', 20, column_formats[column][1])
+        writer.sheets['Recommended Trades'].write(f'{column}1', column_formats[column][0], string_format)
+
+
+    writer.save()
+
+
 stocks_df = get_stocks_df_batch(stock_columns)
-calculate_no_shares_to_buy(stocks_df)
-
-
+stocks_to_buy_df = calculate_no_shares_to_buy(stocks_df)
+generate_xlsx_file(stocks_to_buy_df)
